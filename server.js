93c995cfc0f6bf5d1353907e7e71f6f161c8fb27
Juni-dev <67342586+Juni-dev@users.noreply.github.com>
2020-10-06 14:27:46 +0300

    sending index.html to server main page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.render(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000)
